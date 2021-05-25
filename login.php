413f64fd876caf99f69c6e5fd34254fa5d6bd01c
Thays Casado <thayscasado@hotmail.com>
2021-05-24 23:54:13 -0700

    create login.php file and add bootstrap css link

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Login Sytem PHP</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Hello</h1>
+    </div>
+</body>
+</html>
