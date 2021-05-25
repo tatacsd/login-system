72958ade2d1a78b480c195c1eb48f9ab8d81c594
Thays Casado <thayscasado@hotmail.com>
2021-05-25 00:11:19 -0700

    add bootstrap classes

--- login.php
+++ login.php
@@ -10,8 +10,58 @@
     <title>Login Sytem PHP</title>
 </head>
 <body>
-    <div class="container">
-        <h1>Hello</h1>
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        <form 
+            class="p-5 rounded shadow" 
+            action=""
+            method="post"
+            style="width:30rem">
+            <h1 class="text-center pb-5 display-4">
+                Login
+            </h1>
+            <div class="mb-3">
+                <label 
+                    for="exampleInputEmail1" 
+                    class="form-label">
+                    Email address
+                </label>
+                <input  
+                    type="email" 
+                    class="form-control" 
+                    id="exampleInputEmail1" 
+                    aria-describedby="emailHelp"
+                >
+            </div>
+            <div class="mb-3">
+                <label 
+                    for="exampleInputPassword1" 
+                    class="form-label">
+                    Password
+                </label>
+                <input 
+                    type="password" 
+                    class="form-control" 
+                    id="exampleInputPassword1"
+                >
+            </div>
+            <div class="mb-3 form-check">
+                <input 
+                    type="checkbox" 
+                    class="form-check-input" 
+                    id="exampleCheck1"
+                >
+                <label 
+                    class="form-check-label" 
+                    for="exampleCheck1">
+                    Check me out
+                </label>
+            </div>
+            <button 
+                type="submit" 
+                class="btn btn-primary">
+                Submit
+            </button>
+        </form>
     </div>
 </body>
 </html>
